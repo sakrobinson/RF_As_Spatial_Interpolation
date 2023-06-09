a24c9fb15b852c05e42bb6d555b3e5f92a7559a2
sakrobinson <70336185+sakrobinson@users.noreply.github.com>
2023-06-09 16:51:21 -0600

    Create RF_interpolation.py
    script that uses scikit-learn's Random Forest Classifier to classify the "INCOME" value from a survey dataset "survey_data.csv" using only the "LATITUDE" and "LONGITUDE" fields as input features. The script then generates random spatial points and uses the trained model to predict the "INCOME" value for those new points

---
+++ RF_interpolation.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+
+# Load the survey data into a pandas dataframe
+survey_data = pd.read_csv("survey_data.csv")
+
+# Select the input features and target variable
+X = survey_data[["LATITUDE", "LONGITUDE"]]
+y = survey_data["INCOME"]
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train a Random Forest Classifier on the training data
+rfc = RandomForestClassifier(n_estimators=100, random_state=42)
+rfc.fit(X_train, y_train)
+
+# Generate random spatial points
+n_points = 100
+min_lat, max_lat = X["LATITUDE"].min(), X["LATITUDE"].max()
+min_lon, max_lon = X["LONGITUDE"].min(), X["LONGITUDE"].max()
+random_lats = np.random.uniform(min_lat, max_lat, n_points)
+random_lons = np.random.uniform(min_lon, max_lon, n_points)
+new_points = pd.DataFrame({"LATITUDE": random_lats, "LONGITUDE": random_lons})
+
+# Use the trained model to predict the "INCOME" value for the new points
+predictions = rfc.predict(new_points)
+
+# Print the predicted "INCOME" values for the new points
+print(predictions)
